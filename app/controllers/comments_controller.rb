0e0b0cdd58b9582b55aa236cdb4b2b9a9e2af3a6
uenobu106 <uenobu106@ezweb.ne.jp>
2017-07-23 15:14:56 +0900

    コメント編集機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
   # コメントを保存、投稿するためのアクションです。
   def create
     # Topicをパラメータの値から探し出し,Topicに紐づくcommentsとしてbuildします。
@@ -6,15 +8,26 @@ class CommentsController < ApplicationController
     @topic = @comment.topic
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
-      if @comment.save
-        format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
-        # JS形式でレスポンスを返します。
-        format.js { render :index }
-      else
-        format.html { render :new }
+        if @comment.save
+          format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
+          # JS形式でレスポンスを返します。
+          format.js { render :index }
+        else
+          format.html { render :new }
+        end
       end
     end
-  end
+
+    def edit
+    end
+
+    def update
+      if @comment.update(comment_params)
+      redirect_to topic_path(@topic)
+      else
+      render :edit
+      end
+    end
 
     def destroy
      @comment = Comment.find(params[:id])
@@ -25,9 +38,13 @@ class CommentsController < ApplicationController
      end
    end
 
-  private
-    # ストロングパラメーター
+   private
     def comment_params
       params.require(:comment).permit(:topic_id, :content)
     end
-end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+      @topic = @comment.topic
+    end
+   end
