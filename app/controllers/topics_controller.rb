47e83941246b385ebd9ec0cd97e43ecf49713c2c
uenobu106 <uenobu106@ezweb.ne.jp>
2017-07-24 19:01:31 +0900

    topic改善

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,81 +1,67 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
-  # GET /topics
-  # GET /topics.json
   def index
     @topics = Topic.all
-    respond_to do |format|
-    format.html
-    format.js
-   end
   end
 
-  # GET /topics/1
-  # GET /topics/1.json
   def show
     @comment = @topic.comments.build
     @comments = @topic.comments
   end
 
-  # GET /topics/new
+
   def new
     @topic = Topic.new
+    if params[:back]
+      @topic = Topic.new(topics_params)
+    else
+      @topic = Topic.new
+    end
   end
 
-  # GET /topics/1/edit
+
   def edit
+    @topic = Topic.find(params[:id])
   end
 
-  # POST /topics
-  # POST /topics.json
+
   def create
-    @topic = Topic.new(topic_params)
+    @topic = Topic.new(topics_params)
     @topic.user_id = current_user.id
-    respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topicを作成しました' }
-        format.json { render :show, status: :created, location: @topic }
+        redirect_to topics_path, notice: 'Topicを作成しました'
         #deliverメソッドでNoticeMailerのsendmail_topicメソッドを呼び出す。
         NoticeMailer.sendmail_topic(@topic).deliver
       else
         render :new
       end
-    end
   end
 
-  # PATCH/PUT /topics/1
-  # PATCH/PUT /topics/1.json
+
   def update
-    respond_to do |format|
-      if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
-        format.json { render :show, status: :ok, location: @topic }
-      else
-        format.html { render :edit }
-        format.json { render json: @topic.errors, status: :unprocessable_entity }
-      end
+    @topic = Topic.find(params[:id])
+    if @topic.update(topics_params)
+      redirect_to topics_path, notice: "Topicを更新しました！"
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /topics/1
-  # DELETE /topics/1.json
+
   def destroy
+    @topic = Topic.find(params[:id])
     @topic.destroy
-    respond_to do |format|
-      format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to topics_path,notice: "Topicを削除しました！"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_topic
-      @topic = Topic.find(params[:id])
+    def topics_params
+      params.require(:topic).permit(:title, :content)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def topic_params
-      params.require(:topic).permit(:title, :content)
+    def set_topic
+      @topic = Topic.find(params[:id])
     end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
+  validates :title,:content, presence: true
   belongs_to :user
   # CommentモデルのAssociationを設定
   has_many :comments, dependent: :destroy
