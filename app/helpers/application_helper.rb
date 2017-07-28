79e7a5957191940d63bda77d820acd5d90832b53
uenobu106 <uenobu106@ezweb.ne.jp>
2017-07-28 20:01:31 +0900

    seed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       unless user.provider.blank?
         img_url = user.image_url
       else
-        img_url = 'no_image.png'
+        img_url = 'no_image.jpg'
       end
         image_tag(img_url, alt: user.name)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times do
+email = Faker::Internet.email
+name = Faker::LordOfTheRings.character
+password = "password"
+avatar = Faker::Avatar.image
+user = User.new(
+email: email,
+name: name,
+password: password,
+password_confirmation: password,
+avatar: avatar,
+uid: SecureRandom.uuid,
+)
+user.skip_confirmation!
+user.save
+end
+
+130.times do
+  follower_id = [*1..20].sample
+  followed_id = [*1..20].sample
+  while follower_id == followed_id
+    followed_id = [*1..20].sample
+  end
+    Relationship.find_or_create_by(
+    follower_id: follower_id,
+    followed_id: followed_id
+  )
+end
+
+10.times do
+  content = Faker::LordOfTheRings.character
+  user_id = [*1..10].sample
+  Topic.create(
+  content: content,
+  user_id: user_id,
+  )
+end
+
+
+20.times do
+  user_id = [*1..10].sample
+  topic_id = [*1..10].sample
+  content = Faker::LordOfTheRings.character
+  Comment.create!(
+    user_id: user_id,
+    topic_id: topic_id,
+    content: content
+  )
+end
