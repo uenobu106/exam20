014a8a3fefa413e263008b9edc0e8caddadd7efd
uenobu106 <uenobu106@ezweb.ne.jp>
2017-07-30 15:29:12 +0900

    messege修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   before_action do
     @conversation = Conversation.find(params[:conversation_id])
   end
-  
+
     def index
     @messages = @conversation.messages
     #もし10件を超えるならば最新の10件を表示する
@@ -31,6 +31,8 @@ class MessagesController < ApplicationController
     @message = @conversation.messages.build(message_params)
     #保存ができれば、会話にひもづくメッセージ一覧の画面に遷移する
     if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    else
       redirect_to conversation_messages_path(@conversation)
     end
   end
